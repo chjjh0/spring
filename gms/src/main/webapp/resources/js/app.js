f5c772b7bc3c8b758ee4c27adb1324e25e72557d
chjjh0 <chjjh0@gmail.com>
2018-08-23 03:08:40 +0900

    0823

---
+++ gms/src/main/webapp/resources/js/app.js
@@ -0,0 +1,28 @@
+"use strict";
+var app = app || {};
+app = {
+		init : x =>{	
+			alert("step1");
+			app.session.context(x);
+			app.onCreate();
+		},
+		onCreate : ()=>{
+			alert("step3");
+			app.setContentView();
+		},
+		setContentView : ()=>{
+			alert("step4 : "+app.session.path("js"));
+		}
+};
+app.session = {
+		context : x=>{
+			alert("step2 : "+x);
+			sessionStorage.setItem("context",x);
+			sessionStorage.setItem("css",x+"/resources/css");
+			sessionStorage.setItem("img",x+"/resources/img");
+			sessionStorage.setItem("js",x+"/resources/js");
+		},
+		path : x=>{
+			return sessionStorage.getItem(x);
+		}
+}
