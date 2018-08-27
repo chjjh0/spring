ed20916403005b75ce0a3bef48a1b19f6b4cd489
chjjh0 <chjjh0@gmail.com>
2018-08-27 22:22:38 +0900

    0827 last

---
+++ gms/src/main/java/com/gms/web/controller/MemberController.java
@@ -0,0 +1,50 @@
+package com.gms.web.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gms.web.domain.MemberDTO;
+import com.gms.web.service.MemberService;
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+	static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	@Autowired MemberDTO member;
+	@Autowired MemberService memberService;
+	@RequestMapping("/add")
+	public void add() {}
+	@RequestMapping("/list")
+	public void list() {}
+	@RequestMapping("/search")
+	public void search() {}
+	@RequestMapping("/retrieve")
+	public void retrieve() {}
+	@RequestMapping("/count")
+	public void count() {}
+	@RequestMapping("/modify")
+	public void modify() {}
+	@RequestMapping("/remove")
+	public void remove() {}
+	@RequestMapping("/login")
+	public String login() {
+		logger.info("MembeController login");
+		Map<String, String> p = new HashMap<>();
+		p.put("memberId", "CHJJH0");
+		MemberDTO m = memberService.retrieve(p);
+		System.out.println("getName(): "+m.getName());
+		return "retrieve:member/retrieve.tiles";
+	}
+	@RequestMapping("/logout")
+	public String logout() {
+		logger.info("MembeController logout");
+		return "redirect:/";
+	}
+	@RequestMapping("/fileupload")
+	public void fileupload() {}
+}
---
+++ gms/src/main/webapp/resources/js/app.js
@@ -0,0 +1,60 @@
+"use strict";
+var app = app || {};
+app = {
+		init : x =>{	
+			console.log("step1");
+			app.session.context(x);
+			app.onCreate(x);
+		},
+		onCreate : ()=>{
+			console.log("step3");
+			app.setContentView();
+			$('#login').click(()=>{
+				alert('Login Page');
+				location.href = app.x()+"/move/login/member/login";
+			});
+			$('#login_btn').click(()=>{
+				alert('Login Succes!!!');
+				location.href = app.x()+"/member/login";
+			});
+			$('#logout').click(()=>{
+				alert('logout...');
+				location.href = app.x()+"/member/logout";
+			});
+			$('#join').click(()=>{
+				alert('Join Page');
+				location.href = app.x()+"/move/join/member/add";
+			});
+			$('#join_bth').click(()=>{
+				alert('Add Succes');
+				location.href = app.x();
+			});
+		},
+		setContentView : ()=>{
+			console.log("step4 : "+ app.j());
+		}
+};
+app.session = {
+		context : x=>{
+			console.log("step2 : "+x);
+			sessionStorage.setItem("context",x);
+			sessionStorage.setItem("css",x+"/resources/css");
+			sessionStorage.setItem("img",x+"/resources/img");
+			sessionStorage.setItem("js",x+"/resources/js");
+		},
+		path : x=>{
+			return sessionStorage.getItem(x);
+		}
+};
+app.x = ()=>{
+	return app.session.path('context');
+};
+app.j = ()=>{
+	return app.session.path('js');
+};
+app.c = ()=>{
+	return app.session.path('css');
+};
+app.i = ()=>{
+	return app.session.path('img');
+};
